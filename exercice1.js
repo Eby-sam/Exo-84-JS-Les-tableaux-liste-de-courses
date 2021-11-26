7a1f7bdb2e990a25a833803e70c2769094449a18
coque <coquelet.samuel@gmail.com>
2021-11-26 10:45:45 +0100

    fini

--- exercice1.js
+++ exercice1.js
@@ -12,8 +12,33 @@ let articles = [
 
 // Votre code à partir d'ici :
 
+let listeCourse = document.getElementById("listeCourse")
+let liste = document.createElement('li');
+articles.push("Chocolat", "Coca-Cola")
+liste.innerHTML = articles ;
+listeCourse.appendChild(liste);
+
+let button = document.getElementById("button1");
 
 
+//Ajouter un article à la liste de course
+
+let add = function () {
+    let input1 = document.getElementById("input1").value;
+    articles.push(input1);
+    liste.innerHTML = articles;
+    listeCourse.appendChild(liste);
+}
+button.addEventListener("click", add);
+
+
+// supprimer le dernier article de la liste de course.
+
+let supprimer = document.getElementById("button2");
+supprimer.addEventListener("click", function () {
+    articles.pop();
+    liste.innerHTML = articles;
+});
 
 
 
